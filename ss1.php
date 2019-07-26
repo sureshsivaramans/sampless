e768fda4c623ed4d6948effdc99e74cb0fe265a0
sureshsivaramans <53297442+sureshsivaramans@users.noreply.github.com>
2019-07-26 12:38:25 +0530

    Add files via upload

---
+++ ss1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xml:lang='en' xmlns='http://www.w3.org/1999/xhtml'>
+  <head>
+
+    
+    <title>PHP Example Application for PostageApp</title>
+  </head>
+  
+  <body>
+     <?php 
+	 
+	 echo 'Hello this is  My First PHP Script';
+		 
+	 ?>
+  </body>
+</html>
